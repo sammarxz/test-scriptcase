4d3af2e3c3241c0e853eaecd8721f64b4194a79a
Sam Marxz <sammarxzz@gmail.com>
2020-11-16 11:52:53 -0300

    Add minimal reset. Add Inter font-family.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = () => (
-  <h1>Home</h1>
-);
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Hello World',
+    };
+  }
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <h1>{name}</h1>
+    );
+  }
+}
 
 export default Home;
